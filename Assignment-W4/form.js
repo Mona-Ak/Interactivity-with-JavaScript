2bfaf426a31a54b8af9eea6ad0c74fa27b4fb12d
Mona <monaakbarpour1@gmail.com>
2021-05-04 21:35:49 +0200

    I made a function that fill the inputs automatically with just clicking a box

--- Assignment-W4/form.js
+++ Assignment-W4/form.js
@@ -1,1 +1,24 @@
 /*Add the JavaScript here for the function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
+
+function billingFunction(){
+    let shipName = document.getElementById('shippingName');
+    let shipZip = document.getElementById('shippingZip');
+    let checkbox = document.getElementById('same');
+    
+    let billName = document.getElementById('billingName');
+    let billZip = document.getElementById('billingZip')
+
+    if(checkbox.checked){
+        billName.value = shipName.value
+        billZip.value = shipZip.value
+    }else{
+        billName.value = '';
+        billZip.value = '';
+    }
+     
+    
+
+}
+
+
+
